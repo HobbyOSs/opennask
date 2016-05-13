8defed2365993079a2ed163e0e1c84e6bbab8d9d
hiroyuki nagata <newserver002@gmail.com>
2016-05-14 01:12:10 +0900

    Add test for one-line comments

--- main.cpp
+++ main.cpp
@@ -30,21 +30,23 @@ int main(int argc, char** argv)
      }
 
      // 各文法要素テーブルの生成
-     TParaCxxTokenTable TokenTable;
-     TParaCxxOperatorTable OperatorTable;
-     TParaObjectPrototypeTable ObjectPrototypeTable;
-     TParaBuiltinFunctionTable BuiltinFunctionTable;
-     TParaSymbolTable SymbolTable(&ObjectPrototypeTable, &BuiltinFunctionTable);
+     TParaCxxTokenTable token_table;
+     token_table.AddOperator(";");
+
+     TParaCxxOperatorTable operator_table;
+     TParaObjectPrototypeTable object_prototype_table;
+     TParaBuiltinFunctionTable builtin_function_table;
+     TParaSymbolTable SymbolTable(&object_prototype_table, &builtin_function_table);
 
      /* 組み込み関数テーブルに数学関数を追加 */
-     BuiltinFunctionTable.RegisterAnonymousClass(new TParaMathObject);
+     builtin_function_table.RegisterAnonymousClass(new TParaMathObject);
 
      /* 演算子テーブルに冪乗演算子 (**) を追加 */
-     //OperatorTable.AddOperator(
+     //operator_table.AddOperator(
      // 	  "**", TParaOperatorPriority("*", -1), new TParaOperatorPower()
      // 	  );
-     TParaExpressionParser ExpressionParser(&OperatorTable);
-     TokenTable.AddOperator("**");
+     TParaExpressionParser ExpressionParser(&operator_table);
+     token_table.AddOperator("**");
 
      /* シンボルテーブルに定義済み変数 pi (円周率), e (自然対数の底), x を追加 */
      // SymbolTable.RegisterVariable("pi", TParaValue(3.141592));
@@ -55,24 +57,22 @@ int main(int argc, char** argv)
      std::string input;
      while (nas_file && std::getline(nas_file, input)) {
 	  /* 入力行を istream にしてトークナイザを生成 */
-	  std::istrstream InputStream(input.c_str());
-	  TParaTokenizer Tokenizer(InputStream, &TokenTable);
+	  std::istrstream input_stream(input.c_str());
+	  TParaTokenizer tokenizer(input_stream, &token_table);
+	  TParaToken token;
 
-	  TParaExpression* Expression = 0;
-	  try {
-	       /* トークナイザを渡して，入力を解析 */
-	       Expression = ExpressionParser.Parse(&Tokenizer, &SymbolTable);
+	  while (! (token = tokenizer.Next()).IsEmpty()) {
+	       std::cout << "eval: " << token.AsString() << std::endl;
 
-	       /* 解析に成功したら，評価 */
-	       TParaValue Value = Expression->Evaluate(&SymbolTable);
-	       std::cout << Value.AsString() << std::endl;
+	       if (token.Is(";") || token.Is("#")) {
+		    std::cout << "it's comments" << std::endl;
+		    break;
+	       } else if (token.Is(",")) {
+		    continue;
+	       } else {
+		    std::cout << "opecode or imm" << std::endl;
+	       }
 	  }
-	  catch (TScriptException &e) {
-	       std::cerr << "ERROR: " << e << std::endl;
-	  }
-
-	  /* Parse() で生成された式オブジェクトは毎回手動で delete する */
-	  delete Expression;
      }
 
      std::cout << std::endl;
