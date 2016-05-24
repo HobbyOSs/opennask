072afc9e7594e9fcbf1efbe303dfc4b778d288f1
hiroyuki nagata <newserver002@gmail.com>
2016-05-24 21:04:19 +0900

    Move main sources into src dir

--- projects/automake.sh
+++ projects/automake.sh
@@ -11,19 +11,21 @@ do
     FILEPATH=$FILE
     FILENAME=${FILE%.*}
     FILE_DIR=`dirname ${FILEPATH}`
-    BIN_NAME=`basename ${FILENAME}`
+    INDEX=`basename ${FILE_DIR}`
+    BIN_NAME="${INDEX}_"`basename ${FILENAME}`
+    SRC_NAME=`basename ${FILENAME}`
 
-    echo -e "#"                                             > ${FILE_DIR}/Makefile.am
-    echo -e "# Generated `date` "                          >> ${FILE_DIR}/Makefile.am
-    echo -e "#"                                            >> ${FILE_DIR}/Makefile.am
+    echo -e "#"                                                 > ${FILE_DIR}/Makefile.am
+    echo -e "# Generated `date` "                              >> ${FILE_DIR}/Makefile.am
+    echo -e "#"                                                >> ${FILE_DIR}/Makefile.am
+    echo -e ""                                                 >> ${FILE_DIR}/Makefile.am
+    echo -e "NASK = \$(top_builddir)/src/opennask\$(EXEEXT)"   >> ${FILE_DIR}/Makefile.am
     echo -e ""                                             >> ${FILE_DIR}/Makefile.am
-    echo -e "NASK = \$(top_builddir)/opennask\$(EXEEXT)"   >> ${FILE_DIR}/Makefile.am
+    echo -e "bin_PROGRAMS = ${NASK} ${BIN_NAME}.img"       >> ${FILE_DIR}/Makefile.am
+    echo -e "${BIN_NAME}_img_SOURCES = ${SRC_NAME}.nas"    >> ${FILE_DIR}/Makefile.am
     echo -e ""                                             >> ${FILE_DIR}/Makefile.am
-    echo -e "all:"                                         >> ${FILE_DIR}/Makefile.am
-    echo -e "\t\t\$(MAKE) ${BIN_NAME}.img"                 >> ${FILE_DIR}/Makefile.am
-    echo -e ""                                             >> ${FILE_DIR}/Makefile.am
-    echo -e "${BIN_NAME}.img : ${BIN_NAME}.nas"            >> ${FILE_DIR}/Makefile.am
-    echo -e "\t\t\$(NASK) ${BIN_NAME}.nas ${BIN_NAME}.img" >> ${FILE_DIR}/Makefile.am
+    echo -e "${BIN_NAME}.img\$(EXEEXT) : ${SRC_NAME}.nas"  >> ${FILE_DIR}/Makefile.am
+    echo -e "\t\t\$(NASK) ${SRC_NAME}.nas ${BIN_NAME}.img" >> ${FILE_DIR}/Makefile.am
     echo -e ""                                             >> ${FILE_DIR}/Makefile.am
 
     AC_CONFIG_FILE=`echo ${FILE_DIR}/Makefile | sed -e 's/^\./projects/g'`
