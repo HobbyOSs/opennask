45e94e0e0738e347c794c19d4ff256d6ac7ee439
hiroyuki nagata <newserver002@gmail.com>
2016-05-24 20:03:45 +0900

    Add nask sources for 01_day/02_day

---
+++ projects/automake.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+NAS_FILES=(`find . -name \*.nas -print | sort`)
+
+echo "Please add following statements in configure.ac"
+echo "-----------------------------------------------"
+echo "AC_CONFIG_FILES(["
+
+for FILE in ${NAS_FILES[@]}
+do
+    FILEPATH=$FILE
+    FILENAME=${FILE%.*}
+    FILE_DIR=`dirname ${FILEPATH}`
+    BIN_NAME=`basename ${FILENAME}`
+
+    echo -e "#"                                             > ${FILE_DIR}/Makefile.am
+    echo -e "# Generated `date` "                          >> ${FILE_DIR}/Makefile.am
+    echo -e "#"                                            >> ${FILE_DIR}/Makefile.am
+    echo -e ""                                             >> ${FILE_DIR}/Makefile.am
+    echo -e "NASK = \$(top_builddir)/opennask\$(EXEEXT)"   >> ${FILE_DIR}/Makefile.am
+    echo -e ""                                             >> ${FILE_DIR}/Makefile.am
+    echo -e "all:"                                         >> ${FILE_DIR}/Makefile.am
+    echo -e "\t\t\$(MAKE) ${BIN_NAME}.img"                 >> ${FILE_DIR}/Makefile.am
+    echo -e ""                                             >> ${FILE_DIR}/Makefile.am
+    echo -e "${BIN_NAME}.img : ${BIN_NAME}.nas"            >> ${FILE_DIR}/Makefile.am
+    echo -e "\t\t\$(NASK) ${BIN_NAME}.nas ${BIN_NAME}.img" >> ${FILE_DIR}/Makefile.am
+    echo -e ""                                             >> ${FILE_DIR}/Makefile.am
+
+    AC_CONFIG_FILE=`echo ${FILE_DIR}/Makefile | sed -e 's/^\./projects/g'`
+    echo -e "\t${AC_CONFIG_FILE}"
+done
+
+echo "])"
