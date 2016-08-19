4530ecc09b25da012f8e18fb3e1838b66621e7b9
hiroyuki nagata <newserver002@gmail.com>
2016-08-20 00:51:43 +0900

    Rename example_suite.cpp -> nask_suite.cpp

--- test/nask_suite.cpp
+++ test/nask_suite.cpp
@@ -1,18 +1,19 @@
+#include "nask_utility.hpp"
 #include <CppUTest/TestHarness.h>
 
-TEST_GROUP(example_suite)
+TEST_GROUP(nask_suite)
 {
 };
 
 
-TEST(example_suite, testInt)
+TEST(nask_suite, testInt)
 {
      int i = 3;
 
      CHECK_EQUAL(3, i);
 }
 
-TEST(example_suite, testString)
+TEST(nask_suite, testString)
 {
      const char* str = "example";
 
@@ -20,24 +21,17 @@ TEST(example_suite, testString)
      STRCMP_NOCASE_EQUAL("EXAMPLE", str);
 }
 
-TEST(example_suite, testDouble)
+TEST(nask_suite, testDouble)
 {
      double d = 3.67;
 
      DOUBLES_EQUAL(3.64, d, 0.1);
 }
 
-TEST(example_suite, testPointer)
+TEST(nask_suite, testPointer)
 {
      int i = 3;
      int *p2 = &i;
 
      POINTERS_EQUAL(&i, p2);
 }
-
-//TEST(example_suite, testWillFail)
-//{
-//     int wrong = 3;
-//
-//     CHECK_EQUAL(762, wrong); // This will fail!
-//}
