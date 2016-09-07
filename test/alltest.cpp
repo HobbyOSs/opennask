4fecf62c16846456fed386c651d51498a2a5012a
hiroyuki nagata <newserver002@gmail.com>
2016-09-07 22:31:19 +0900

    Fix test/alltest.cpp for Travis

--- test/alltest.cpp
+++ test/alltest.cpp
@@ -5,8 +5,9 @@ int main(int argc, char** argv)
 {
      std::vector<const char*> args(argv, argv + argc); // Insert all arguments
      args.push_back("-v"); // Set verbose mode
-     //args.push_back("-c"); // Set color output (OPTIONAL)
+     args.push_back("-c"); // Set color output (OPTIONAL)
 
      // Run all tests
-     return RUN_ALL_TESTS(args.size(), &args[0]);
+     int i = RUN_ALL_TESTS(args.size(), &args[0]);
+     return i;
 }
