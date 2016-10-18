df8347740aa167928af3137ffb1d4d6ab8047b55
hiroyuki nagata <newserver002@gmail.com>
2016-10-18 23:13:38 +0900

    [nask] Implement generate_modrm

--- src/nask_utility.cpp
+++ src/nask_utility.cpp
@@ -33,9 +33,47 @@ namespace nask_utility {
 	       return "";
 	  };
 
+	  // @param m       : [mod] mods::REG_REG, REG_DISP8, REG_DISP16, REG
+	  // @param dst_reg : [reg] Register with std::string
+	  // @param reg     : [r/m] '/x' with Enum
 	  uint8_t generate_modrm(enum mods m, const std::string& dst_reg, enum reg_field reg) {
+	       //
+	       // Generate ModR/M byte with arguments
+	       // [mod] 2bit
+	       // [reg] 3bit
+	       // [r/m] 3bit
+	       //
 	       std::string modrm = ModRM::MOD_TO_STR.at(m);
-	       modrm += get_rm_from_reg(dst_reg);
+	       if (reg == SLASH_R) {
+		    modrm += get_rm_from_reg(dst_reg);
+	       } else {
+		    switch (reg) {
+		    case SLASH_0:
+			 modrm += "000";
+			 break;
+		    case SLASH_1:
+			 modrm += "001";
+			 break;
+		    case SLASH_2:
+			 modrm += "010";
+			 break;
+		    case SLASH_3:
+			 modrm += "011";
+			 break;
+		    case SLASH_4:
+			 modrm += "100";
+			 break;
+		    case SLASH_5:
+			 modrm += "101";
+			 break;
+		    case SLASH_6:
+			 modrm += "110";
+			 break;
+		    case SLASH_7:
+			 modrm += "111";
+			 break;
+		    }
+	       }
 	       modrm += get_rm_from_reg(dst_reg);
 	       std::bitset<8> bs(modrm);
 	       return bs.to_ulong();
