e7b30a9e40c9ff5b25ffbeb4aed832724c834269
hiroyuki nagata <newserver002@gmail.com>
2016-11-17 23:15:37 +0900

    [nask] Update Characteristics fields according to libnask

--- src/bracket_impl.hpp
+++ src/bracket_impl.hpp
@@ -29,8 +29,8 @@ namespace nask_utility {
 	       log()->info("target: {}, process as Portable Executable", target);
 	       header.machine              = I386MAGIC;
 	       header.numberOfSections     = 0x0003;
-	       header.pointerToSymbolTable = 0x0000008e;
-	       header.numberOfSymbols      = 0x00000009;
+	       header.pointerToSymbolTable = 0x0000008e; /* +0x08: symboltable */
+	       header.numberOfSymbols      = 0x00000009; /* +0x0c: sizeof (symboltable) / 18 */
 
 	       auto ptr = reinterpret_cast<uint8_t*>(&header);
 	       auto buffer = std::vector<uint8_t>{ ptr, ptr + sizeof(header) };
@@ -38,16 +38,16 @@ namespace nask_utility {
 
 	       // section table ".text"
 	       PIMAGE_SECTION_HEADER text = {
-		    { 0x2e, 0x74, 0x65, 0x78, 0x74, 0x00, 0x00, 0x00 },
-		    0x00000000, // unionの分
-		    0x00000000,
-		    0x00000002,
-		    0x0000008c,
-		    0x0000008e,
-		    0x00000000,
-		    0x0000,
-		    0x0000,
-		    0x60100020
+		    { '.', 't', 'e', 'x', 't', 0, 0, 0 /* name */ },
+		    0x00000000, // Misc
+		    0x00000000, // VirtualAddress
+		    0x00000002,	// SizeOfRawData
+		    0x0000008c,	// PointerToRawData
+		    0x0000008e,	// PointerToRelocations
+		    0x00000000,	// PointerToLinenumbers
+		    0x0000,	// NumberOfRelocations
+		    0x0000,	// NumberOfLinenumbers
+		    0x60100020  /* +0x38: flags, default_align = 1 */
 	       };
 
 	       auto text_ptr = reinterpret_cast<uint8_t*>(&text);
@@ -56,16 +56,16 @@ namespace nask_utility {
 
 	       // section table ".data"
 	       PIMAGE_SECTION_HEADER data = {
-		    { 0x2e, 0x64, 0x61, 0x74, 0x61, 0x00, 0x00, 0x00 },
-		    0x00000000, // unionの分
-		    0x00000000,
-		    0x00000000,
-		    0x00000000,
-		    0x00000000,
-		    0x00000000,
-		    0x0000,
-		    0x0000,
-		    0xc0100040
+		    { '.', 'd', 'a', 't', 'a', 0, 0, 0 /* name */ },
+		    0x00000000, // Misc
+		    0x00000000, // VirtualAddress
+		    0x00000000,	// SizeOfRawData
+		    0x00000000,	// PointerToRawData
+		    0x00000000,	// PointerToRelocations
+		    0x00000000,	// PointerToLinenumbers
+		    0x0000,	// NumberOfRelocations
+		    0x0000,	// NumberOfLinenumbers
+		    0xc0100040  /* +0x60: flags, default_align = 1 */
 	       };
 
 	       auto data_ptr = reinterpret_cast<uint8_t*>(&data);
@@ -74,21 +74,27 @@ namespace nask_utility {
 
 	       // section table ".bss"
 	       PIMAGE_SECTION_HEADER bss = {
-		    { 0x2e, 0x62, 0x73, 0x73, 0x00, 0x00, 0x00, 0x00 },
-		    0x00000000, // unionの分
-		    0x00000000,
-		    0x00000000,
-		    0x00000000,
-		    0x00000000,
-		    0x00000000,
-		    0x0000,
-		    0x0000,
-		    0x00000080
+		    { '.', 'b', 's', 's', 0, 0, 0, 0 /* name */ },
+		    0x00000000, // Misc
+		    0x00000000, // VirtualAddress
+		    0x00000000,	// SizeOfRawData
+		    0x00000000,	// PointerToRawData
+		    0x00000000,	// PointerToRelocations
+		    0x00000000,	// PointerToLinenumbers
+		    0x0000,	// NumberOfRelocations
+		    0x0000,	// NumberOfLinenumbers
+		    0xc0100080  /* +0x88: flags, default_align = 1 */
 	       };
 
 	       auto bss_ptr = reinterpret_cast<uint8_t*>(&bss);
 	       auto bss_buffer = std::vector<uint8_t>{ bss_ptr, bss_ptr + sizeof(bss) };
 	       std::copy(bss_buffer.begin(), bss_buffer.end(), back_inserter(binout_container));
+
+	       // FIXME: ここはよくわからないのでとりあえず埋めておく
+	       binout_container.push_back(0x10);
+	       binout_container.push_back(0xc0);
+	       binout_container.push_back(0xf4);
+	       binout_container.push_back(0xc3);
 	  }
      }
 }
