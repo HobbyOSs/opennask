f096942487f6adae8467ed34d043d2735d5132b9
hiroyuki nagata <newserver002@gmail.com>
2016-12-05 20:48:59 +0900

    [ctest] Add test/modrmtest.cpp

--- src/mod_rm.hpp
+++ src/mod_rm.hpp
@@ -12,18 +12,21 @@ namespace nask_utility {
 
 	  using namespace std::regex_constants;
 
-	  //
-	  // レジスタ名は大文字/小文字にかかわらず探す
-	  //
-	  const std::regex rm000("(AL|AX|EAX|.EAX.|.EAX\\+[0-9]{2}.)", ECMAScript | icase);
-	  const std::regex rm001("(CL|CX|ECX|.ECX.|.ECX\\+[0-9]{2}.)", ECMAScript | icase);
-	  const std::regex rm010("(DL|DX|EDX|.EDX.|.EDX\\+[0-9]{2}.)", ECMAScript | icase);
-	  const std::regex rm011("(BL|BX|EBX|.EBX.|.EBX\\+[0-9]{2}.)", ECMAScript | icase);
-	  const std::regex rm101("(CH|BP|EBP)", ECMAScript | icase);
+	  constexpr const char* _000 = "(AL|AX|EAX|\\[EAX]|\\[EAX\\+.*])";
+	  constexpr const char* _001 = "(CL|CX|ECX|\\[ECX]|\\[ECX\\+.*])";
+	  constexpr const char* _010 = "(DL|DX|EDX|\\[EDX]|\\[EDX\\+.*])";
+	  constexpr const char* _011 = "(BL|BX|EBX|\\[EBX]|\\[EBX\\+.*])";
+	  constexpr const char* _101 = "(CH|BP|EBP)";
 
-	  const std::regex regImm08("(AL|CL|DL|BL|AH|CH|DH|BH)", ECMAScript | icase);
-	  const std::regex regImm16("(AX|BX|CX|DX|SI|DI|BP|SP|IP|FLAGS|CS|SS|DS|ES|FS|GS)", ECMAScript | icase);
-	  const std::regex regImm32("(EAX|EBX|ECX|EDX|ESI|EDI|EBP|ESP|EIP|EFLAGS)", ECMAScript | icase);
+	  const std::regex rm000(_000, extended | icase);
+	  const std::regex rm001(_001, extended | icase);
+	  const std::regex rm010(_010, extended | icase);
+	  const std::regex rm011(_011, extended | icase);
+	  const std::regex rm101(_101, extended | icase);
+
+	  const std::regex regImm08("(AL|CL|DL|BL|AH|CH|DH|BH)", extended | icase);
+	  const std::regex regImm16("(AX|BX|CX|DX|SI|DI|BP|SP|IP|FLAGS|CS|SS|DS|ES|FS|GS)", extended | icase);
+	  const std::regex regImm32("(EAX|EBX|ECX|EDX|ESI|EDI|EBP|ESP|EIP|EFLAGS)", extended | icase);
 
 	  const std::string SIB = "100";
 
---
+++ test/modrmtest.cpp
@@ -0,0 +1,42 @@
+#include "nask_utility.hpp"
+#include "mod_rm.hpp"
+#include "string_util.hpp"
+#include "ParaTokenizer.hh"
+#include "ParaOperator.hh"
+#include "ParaExpression.hh"
+#include "ParaSymbolTable.hh"
+#include "ParaMathLibrary.hh"
+#include <CppUTest/TestHarness.h>
+#include <CppUTest/CommandLineTestRunner.h>
+
+// Init stuff
+std::shared_ptr<spdlog::logger> logger = spdlog::basic_logger_mt("opennask", "debug.log");
+
+TEST_GROUP(modrmtest_suite)
+{
+};
+
+TEST(modrmtest_suite, check_32bit_reg_brackets)
+{
+     using namespace nask_utility;
+
+     CHECK(ModRM::get_rm_from_reg("[EAX]") == "000");
+     CHECK(ModRM::get_rm_from_reg("[EAX+]") == "000");
+     CHECK(ModRM::get_rm_from_reg("[EAX+4]") == "000");
+     CHECK(ModRM::get_rm_from_reg("[EAX+12]") == "000");
+
+     CHECK(ModRM::get_rm_from_reg("[EXX-12]") != "000");
+     CHECK(ModRM::get_rm_from_reg("[EAX-12]") != "000");
+     CHECK(ModRM::get_rm_from_reg("[EAXx12]") != "000");
+}
+
+int main(int argc, char** argv)
+{
+     std::vector<const char*> args(argv, argv + argc); // Insert all arguments
+     args.push_back("-v"); // Set verbose mode
+     args.push_back("-c"); // Set color output (OPTIONAL)
+
+     // Run all tests
+     int i = RUN_ALL_TESTS(args.size(), &args[0]);
+     return i;
+}
