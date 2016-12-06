a65fed30f120a330ccff6294fafb93c40dce23e2
hiroyuki nagata <newserver002@gmail.com>
2016-12-07 00:25:59 +0900

    [inst] Implement PUSHFD, PUSHF

--- src/nask_utility.cpp
+++ src/nask_utility.cpp
@@ -2494,6 +2494,13 @@ namespace nask_utility {
 	  return 0;
      }
 
+     // テキトーなPUSHF命令の実装
+     int Instructions::process_token_PUSHF(TParaTokenizer& tokenizer, VECTOR_BINOUT& binout_container) {
+	  // 0x9C を格納
+	  binout_container.push_back(0x9c);
+	  return 0;
+     }
+
      // テキトーなRET命令の実装
      int Instructions::process_token_RET(TParaTokenizer& tokenizer, VECTOR_BINOUT& binout_container) {
 	  // 0xC3 を格納
