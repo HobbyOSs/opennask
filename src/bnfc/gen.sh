fa1b0c6a54871bcf5a45266bf369c44cdee2dbc1
hiroyuki.nagata <idiotpanzer@gmail.com>
2021-08-29 11:58:13 +0900

    Append gen.sh nask.cf

--- src/bnfc/gen.sh
+++ src/bnfc/gen.sh
@@ -13,4 +13,17 @@ find . -name \*.cc -or -name \*.hh -or -name \*.l -or -name \*.y | xargs sed -i 
 # https://github.com/BNFC/bnfc/issues/377
 sed -i '6i #include<algorithm>' absyn.hh
 sed -i 's/%pure_parser/%define api.pure/g' nask.y
+sed -i 's/%option noyywrap noinput nounput/%option noyywrap noinput nounput debug/g' nask.l
+sed -i 's/return scanner;/nask_set_debug(true, scanner); return scanner;/g' nask.l
+
+# to skip '\n' after one-line-comment, I wonder why this regex is not working...??
+# BEFORE:
+# <INITIAL>"#"[^\n]* /* skip */; /* BNFC: comment "#" */
+# <INITIAL>";"[^\n]* /* skip */; /* BNFC: comment ";" */
+# AFTER:
+# <INITIAL>"#"[.] /* skip */; /* BNFC: comment "#" */
+# <INITIAL>";"[.] /* skip */; /* BNFC: comment ";" */
+sed -i 's/\/\* skip \*\//\/\* skip \*\/ return _SYMB_0;/g' nask.l
+sed -i 's/<INITIAL>\[ \\t\\r\\n\\f\]/<INITIAL>\[ \\t\\r\\f\]/g' nask.l
+
 make
