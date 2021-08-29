9b53bab9ef59ecf911ccd345420c3d84bbd7d363
hiroyuki.nagata <idiotpanzer@gmail.com>
2021-08-29 12:58:20 +0900

    Fix lexer with gen.sh

--- src/bnfc/gen.sh
+++ src/bnfc/gen.sh
@@ -13,17 +13,28 @@ find . -name \*.cc -or -name \*.hh -or -name \*.l -or -name \*.y | xargs sed -i 
 # https://github.com/BNFC/bnfc/issues/377
 sed -i '6i #include<algorithm>' absyn.hh
 sed -i 's/%pure_parser/%define api.pure/g' nask.y
+
+# flexでdebugログ出す設定
 sed -i 's/%option noyywrap noinput nounput/%option noyywrap noinput nounput debug/g' nask.l
 sed -i 's/return scanner;/nask_set_debug(true, scanner); return scanner;/g' nask.l
 
-# to skip '\n' after one-line-comment, I wonder why this regex is not working...??
+# comment要素追加
+sed -i 's/%START CHAR CHARESC CHAREND STRING ESCAPED/%START CHAR CHARESC CHAREND STRING ESCAPED COMMENT/g' nask.l
+
+# commentのstate設定
 # BEFORE:
 # <INITIAL>"#"[^\n]* /* skip */; /* BNFC: comment "#" */
 # <INITIAL>";"[^\n]* /* skip */; /* BNFC: comment ";" */
 # AFTER:
 # <INITIAL>"#"[.] /* skip */; /* BNFC: comment "#" */
 # <INITIAL>";"[.] /* skip */; /* BNFC: comment ";" */
-sed -i 's/\/\* skip \*\//\/\* skip \*\/ return _SYMB_0;/g' nask.l
+sed -i 's/\/\* skip \*\/;/\/\* skip \*\/ BEGIN COMMENT;/g' nask.l
+
+#sed -i "/<INITIAL>\";\"/an'<COMMENT>\n		BEGIN INITIAL;'" < nask.l
+#sed -i "/<INITIAL>\";\"/an'<COMMENT>[^\n]+                     ;'" < nask.l
+
+sed -i "s/\(<INITIAL>\";\".*\)/\1\n<COMMENT>\\\\n		BEGIN INITIAL;\n<COMMENT>\[^\\\\n\]+                     ;\n/g" nask.l
+
 sed -i 's/<INITIAL>\[ \\t\\r\\n\\f\]/<INITIAL>\[ \\t\\r\\f\]/g' nask.l
 
 make
