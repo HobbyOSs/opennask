dd135c4b75fc640f1407fe34d5c15dad2efd4266
hiroyuki.nagata <idiotpanzer@gmail.com>
2021-09-20 19:23:35 +0900

    Finish day01

--- test/day01test.cpp
+++ test/day01test.cpp
@@ -192,7 +192,7 @@ int main(int argc, char** argv) {
     std::vector<const char*> args(argv, argv + argc); // Insert all arguments
     args.push_back("-v"); // Set verbose mode
     args.push_back("-c"); // Set color output (OPTIONAL)
-    args.push_back("TEST(day01_suite, helloos2)");
+    //args.push_back("TEST(day01_suite, helloos2)");
 
     // Run all tests
     int i = RUN_ALL_TESTS(args.size(), &args[0]);
