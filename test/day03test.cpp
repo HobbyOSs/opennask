e1f68712c73cb7d83fa86d63fca1aea3755aeb8e
hiroyuki.nagata <idiotpanzer@gmail.com>
2021-10-07 07:52:00 +0900

    Fix testcase

--- test/day03test.cpp
+++ test/day03test.cpp
@@ -416,25 +416,25 @@ fin:
 		JMP		fin				; 無限ループ
 
 error:
-		;MOV		SI,msg
+		MOV		SI,msg
 putloop:
-		;MOV		AL,[SI]
-		;ADD		SI,1			; SIに1を足す
-		;CMP		AL,0
-		;JE		fin
-		;MOV		AH,0x0e			; 一文字表示ファンクション
-		;MOV		BX,15			; カラーコード
-		;INT		0x10			; ビデオBIOS呼び出し
-		;JMP		putloop
-;msg:
-		;DB		0x0a, 0x0a		; 改行を2つ
-		;DB		"load error"
-		;DB		0x0a			; 改行
-		;DB		0
+		MOV		AL,[SI]
+		ADD		SI,1			; SIに1を足す
+		CMP		AL,0
+		JE		fin
+		MOV		AH,0x0e			; 一文字表示ファンクション
+		MOV		BX,15			; カラーコード
+		INT		0x10			; ビデオBIOS呼び出し
+		JMP		putloop
+msg:
+		DB		0x0a, 0x0a		; 改行を2つ
+		DB		"load error"
+		DB		0x0a			; 改行
+		DB		0
 
-		;RESB	0x7dfe-$		; 0x7dfeまでを0x00で埋める命令
+		RESB	0x7dfe-$		; 0x7dfeまでを0x00で埋める命令
 
-		;DB		0x55, 0xaa
+		DB		0x55, 0xaa
 )";
 
     // od形式で出力する際は `od -t x1 test/test.img > test_img.txt`
@@ -443,7 +443,7 @@ putloop:
 
     std::vector<uint8_t> expected = {};
     std::vector<uint8_t> resb18(18, 0);
-    std::vector<uint8_t> resb339(339, 0);
+    std::vector<uint8_t> resb324(324, 0);
 
     // 以下は標準的なFAT12フォーマットフロッピーディスクのための記述
     expected.insert(expected.end(), {0xeb, 0x4e});
@@ -503,24 +503,23 @@ putloop:
     // 読み終わったけどとりあえずやることないので寝る
     expected.insert(expected.end(), {0xf4});
     expected.insert(expected.end(), {0xeb, 0xfd});
-    //expected.insert(expected.end(), {0xbe, 0x00}); // 0xac
-    // expected.insert(expected.end(), {0xbe, 0x9d, 0x7c});
-    // expected.insert(expected.end(), {0x8a, 0x04});
-    // expected.insert(expected.end(), {0x83, 0xc6, 0x01});
-    // expected.insert(expected.end(), {0x3c, 0x00});
-    // expected.insert(expected.end(), {0x74, 0xf1});
-    // expected.insert(expected.end(), {0xb4, 0x0e});
-    // expected.insert(expected.end(), {0xbb, 0x0f, 0x00});
-    // expected.insert(expected.end(), {0xcd, 0x10});
-    // expected.insert(expected.end(), {0xeb, 0xee});
+    expected.insert(expected.end(), {0xbe, 0xac, 0x7c}); // 0xbeac7c
+    expected.insert(expected.end(), {0x8a, 0x04});
+    expected.insert(expected.end(), {0x83, 0xc6, 0x01});
+    expected.insert(expected.end(), {0x3c, 0x00});
+    expected.insert(expected.end(), {0x74, 0xf1});
+    expected.insert(expected.end(), {0xb4, 0x0e});
+    expected.insert(expected.end(), {0xbb, 0x0f, 0x00});
+    expected.insert(expected.end(), {0xcd, 0x10});
+    expected.insert(expected.end(), {0xeb, 0xee});
 
-    // expected.insert(expected.end(), {0x0a, 0x0a});
-    // expected.insert(expected.end(), {0x6c, 0x6f, 0x61, 0x64, 0x20, 0x65, 0x72, 0x72, 0x6f, 0x72});
+    expected.insert(expected.end(), {0x0a, 0x0a});
+    expected.insert(expected.end(), {0x6c, 0x6f, 0x61, 0x64, 0x20, 0x65, 0x72, 0x72, 0x6f, 0x72});
 
-    // expected.insert(expected.end(), {0x0a});
-    // expected.insert(expected.end(), {0x00});
-    // expected.insert(expected.end(), std::begin(resb339), std::end(resb339));
-    // expected.insert(expected.end(), {0x55, 0xaa});
+    expected.insert(expected.end(), {0x0a});
+    expected.insert(expected.end(), {0x00});
+    expected.insert(expected.end(), std::begin(resb324), std::end(resb324));
+    expected.insert(expected.end(), {0x55, 0xaa});
 
     CHECK_EQUAL(expected.size(), d->binout_container.size());
     CHECK_TRUE(std::equal(expected.begin(), expected.end(), d->binout_container.begin()));
@@ -532,7 +531,7 @@ int main(int argc, char** argv) {
     std::vector<const char*> args(argv, argv + argc); // Insert all arguments
     args.push_back("-v"); // Set verbose mode
     args.push_back("-c"); // Set color output (OPTIONAL)
-    args.push_back("TEST(day03_suite, harib00c)");
+    //args.push_back("TEST(day03_suite, harib00c)");
 
     // Run all tests
     int i = RUN_ALL_TESTS(args.size(), &args[0]);
