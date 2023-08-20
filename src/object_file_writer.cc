7ab78cf59adf9c4d31c387d5b1f2d3da49fca50b
hiroyuki.nagata <idiotpanzer@gmail.com>
2023-08-20 16:38:10 +0900

    fix up

--- src/object_file_writer.cc
+++ src/object_file_writer.cc
@@ -21,12 +21,27 @@ ObjectFileWriter::ObjectFileWriter() {
 void ObjectFileWriter::set_file_name(std::string& file_name) {
     // coffiでは`auxiliary_symbol_record_4` で定義される
     // https://coffi.readthedocs.io/en/latest/annotated.html
+    // ちょっとしつこいコメントになるが設定値の意味も付記する
     symbol* sym1 = writer_->add_symbol(".file");
+    // IMAGE_SYM_DEBUG
+    // The symbol provides general type or debugging information but does not correspond to a section.
+    // Microsoft tools use this setting along with .file records (storage class FILE).
+    sym1->set_section_number(IMAGE_SYM_DEBUG);
+
+    // IMAGE_SYM_CLASS_FILE
+    // Used by Microsoft tools, as well as traditional COFF format, for the source-file symbol record.
+    // The symbol is followed by auxiliary records that name the file.
+    sym1->set_storage_class(IMAGE_SYM_CLASS_FILE);
+    // シンボルはこの中では1つしかないので1
+    sym1->set_aux_symbols_number(1);
+
     auxiliary_symbol_record_4 record;
-    std::strncpy(record.file_name, file_name.c_str(), sizeof(record.file_name) - 2);
+    std::strncpy(record.file_name, file_name.c_str(), sizeof(record.file_name));
     for (size_t i = file_name.size(); i < sizeof(record.file_name) - 1; i++) {
         record.file_name[i] = '\0';
     }
+    record.file_name[sizeof(record.file_name) - 1] = '\0'; // 末尾を0で埋める
+
     sym1->get_auxiliary_symbols().push_back(*(auxiliary_symbol_record*)&record);
 }
 
@@ -36,17 +51,6 @@ void ObjectFileWriter::write_coff(asmjit::CodeHolder& code_, asmjit::x86::Assemb
 
     // .textに最終的な機械語を差し込む
     section* text = writer_->add_section(".text");
-    //char text[] = {
-    //    '\x55',                                 // push   %ebp
-    //    '\x89', '\xE5',                         // mov    %esp,%ebp
-    //    '\x83', '\xE4', '\xF0',                 // and    $0xfffffff0,%esp
-    //    '\xE8', '\x00', '\x00', '\x00', '\x00', // call   b <_main+0xb>
-    //    '\xB8', '\x2A', '\x00', '\x00', '\x00', // mov    $0x2a,%eax
-    //    '\xC9',                                 // leave
-    //    '\xC3',                                 // ret
-    //    '\x90',                                 // nop
-    //    '\x90',                                 // nop
-    //};
     text->set_data(reinterpret_cast<const char*>(buf.data()), buf.size());
     section* data = writer_->add_section(".data");
     section* bss = writer_->add_section(".bss");
