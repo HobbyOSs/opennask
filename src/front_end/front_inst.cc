12575878bbd4a316b7ae5ab9789178ef02910328
hiroyuki.nagata <idiotpanzer@gmail.com>
2023-08-29 23:23:01 +0900

    オペランドサイズプレフィックス判定を修正

--- src/front_end/front_inst.cc
+++ src/front_end/front_inst.cc
@@ -1072,14 +1072,16 @@ void PrefixInfo::set(OPENNASK_MODES bit_mode, TParaToken& dst, TParaToken& src) 
 
     // Operand size prefix
     // オペランドサイズプレフィックスが必要かどうか判定する
+    // dst,srcがあるときは下記のパターンがある
     require_66h = match(bit_mode)(
         pattern | ID_16BIT_MODE | when(dst_attr == TParaToken::ttReg32) = true,
-        pattern | ID_16BIT_MODE | when(src_attr == TParaToken::ttReg32) = true,
-        pattern | ID_16BIT_MODE | when(dst.IsImmediate() && dst.AsInt32() > 0x7fff) = true,
+        pattern | ID_16BIT_MODE | when(src_attr == TParaToken::ttReg32 && dst_attr == TParaToken::ttReg32) = true,
+        pattern | ID_16BIT_MODE | when(src_attr == TParaToken::ttReg32 && dst.IsMem()) = true,
         pattern | ID_16BIT_MODE | when(src.IsImmediate() && src.AsInt32() > 0x7fff) = true,
         pattern | ID_16BIT_MODE = false,
         pattern | ID_32BIT_MODE | when(dst_attr == TParaToken::ttReg16) = true,
-        pattern | ID_32BIT_MODE | when(src_attr == TParaToken::ttReg16) = true,
+        pattern | ID_32BIT_MODE | when(src_attr == TParaToken::ttReg16 && dst_attr == TParaToken::ttReg16) = true,
+        pattern | ID_32BIT_MODE | when(src_attr == TParaToken::ttReg16 && dst.IsMem()) = true,
         pattern | ID_32BIT_MODE = false,
         pattern | _ = false
     );
